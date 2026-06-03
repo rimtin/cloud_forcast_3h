79ad5e47f944fabdd4ae18ac2789099e9e2d96da
rimtin <37169534+rimtin@users.noreply.github.com>
2026-06-03 10:54:17 +0530

    Create cloud-data.js

---
+++ cloud-data.js
@@ -0,0 +1,102 @@
+const cloudForecastData = [
+  {
+    state: "Punjab",
+    areas: [
+      { sno: 1, subdivision: "Punjab", day1: "Clear Sky" }
+    ]
+  },
+
+  {
+    state: "Rajasthan",
+    areas: [
+      { sno: 2, subdivision: "West Rajasthan", day1: "Clear Sky" },
+      { sno: 3, subdivision: "East Rajasthan", day1: "Clear Sky" }
+    ]
+  },
+
+  {
+    state: "Gujarat",
+    areas: [
+      { sno: 4, subdivision: "Saurashtra & Kachh", day1: "Clear Sky" },
+      { sno: 5, subdivision: "Gujarat Region", day1: "Clear Sky" }
+    ]
+  },
+
+  {
+    state: "Uttar Pradesh",
+    areas: [
+      { sno: 6, subdivision: "West Uttar Pradesh", day1: "Clear Sky" },
+      { sno: 7, subdivision: "East Uttar Pradesh", day1: "Clear Sky" }
+    ]
+  },
+
+  {
+    state: "Madhya Pradesh",
+    areas: [
+      { sno: 8, subdivision: "West Madhya Pradesh", day1: "Clear Sky" },
+      { sno: 9, subdivision: "East Madhya Pradesh", day1: "Clear Sky" }
+    ]
+  },
+
+  {
+    state: "Chhattisgarh",
+    areas: [
+      { sno: 10, subdivision: "Chhattisgarh", day1: "Clear Sky" }
+    ]
+  },
+
+  {
+    state: "Maharashtra",
+    areas: [
+      { sno: 11, subdivision: "Madhya Maharashtra", day1: "Clear Sky" },
+      { sno: 12, subdivision: "Marathwada", day1: "Clear Sky" },
+      { sno: 13, subdivision: "Vidarbha", day1: "Clear Sky" }
+    ]
+  },
+
+  {
+    state: "Telangana",
+    areas: [
+      { sno: 14, subdivision: "Telangana", day1: "Clear Sky" }
+    ]
+  },
+
+  {
+    state: "Andhra Pradesh",
+    areas: [
+      { sno: 15, subdivision: "Coastal Andhra Pradesh", day1: "Clear Sky" },
+      { sno: 16, subdivision: "Rayalaseema", day1: "Clear Sky" }
+    ]
+  },
+
+  {
+    state: "Karnataka",
+    areas: [
+      { sno: 17, subdivision: "N.I. Karnataka", day1: "Clear Sky" },
+      { sno: 18, subdivision: "S.I. Karnataka", day1: "Clear Sky" }
+    ]
+  },
+
+  {
+    state: "Tamil Nadu",
+    areas: [
+      { sno: 19, subdivision: "Tamil Nadu & Puducherry", day1: "Clear Sky" }
+    ]
+  }
+];
+
+const cloudCategories = [
+  "Clear Sky",
+  "Low Cloud",
+  "Medium Cloud",
+  "High Cloud",
+  "Overcast"
+];
+
+const cloudColors = {
+  "Clear Sky": "#66CCFF",
+  "Low Cloud": "#028A0F",
+  "Medium Cloud": "#FFF500",
+  "High Cloud": "#FF8A00",
+  "Overcast": "#FF0000"
+};
